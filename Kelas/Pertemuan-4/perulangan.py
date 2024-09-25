2233f5dfb587df8cd888c344f498e122c0c1d8c5
Haykal Makhmud <haikal252005@gmail.com>
2024-09-25 08:48:38 +0800

    pertemuan 4

---
+++ Kelas/Pertemuan-4/perulangan.py
@@ -0,0 +1,67 @@
+# ulang = 10
+# for i in range(ulang):
+#     print("perulangan ke-" + str(i))
+
+
+# simpan = [12, "udin petot", 14.5, True, 'A']
+# for i in simpan:
+#     print(i, end=' ')
+
+
+# for i in range(1, 10, 2): #(awal, akhir, lompatan)
+#     print(i)
+
+
+# for i in range(1, 4):
+#     for j in range(1, 4):
+#         print(f"{i} x {j} = {i * j}")
+#     print()
+
+
+# jawab = 'ya'
+# hitung = 0
+# while(jawab == 'ya'):
+#     hitung += 1
+#     jawab = input("Ulang lagi tidak? ")
+# print(f"Total perulangan: {hitung}")
+
+
+# hitung = 0
+# while True:
+#     hitung += 1
+#     ulang = input("Masih Ingin Mengulang? ")
+#     if ulang == "tidak" or ulang =="Tidak":
+#         break
+# print(f"Total Perulangan: {hitung}")
+
+
+# print("Daftar bilangan ganjil dari 1-10")
+# for i in range(10):
+#     if i % 2 == 0:
+#         continue
+#     print(i)
+
+# bilangan = 0
+# while True:
+#     angka = int(input("Masukkan angka: "))
+#     if angka < 0:
+#         break
+#     bilangan += angka
+# print("total bilangan: ", bilangan)
+
+# for i in range(1,20,3):
+#     if i%2 == 0:
+#         continue
+#     print(i)
+
+# buah = ["apel", "anggur", "jeruk", "pisang", "melon"]
+# buah_baru = []
+# for i in buah:
+#     if "e" in i:
+#         buah_baru.append(i)
+# print(buah_baru)
+
+# buah_baru = [i for i in buah if "a" in i]
+# print(buah_baru)
+
+
